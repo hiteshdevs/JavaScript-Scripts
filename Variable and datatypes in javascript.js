309f7ea082e5bd75ade82ef8451b7ab5a84bceae
Hitesh Bhagwat <111442899+hiteshdevs@users.noreply.github.com>
2022-08-20 00:52:49 +0530

    Add files via upload

---
+++ Variable and datatypes in javascript.js
@@ -0,0 +1,23 @@
+var name = 'hitesh';
+console.log(name);
+
+let no = 22;
+console.log(no);
+
+// variables should be
+
+// fullName
+// full-name
+// full_name
+
+
+var courseName = 'ReactJS Boostrap';
+
+var isLoggedIn = false;
+
+var loggedCount = 30;
+
+var paymentMode;
+
+paymentMode = 'Credit Card';
+console.log(paymentMode);
