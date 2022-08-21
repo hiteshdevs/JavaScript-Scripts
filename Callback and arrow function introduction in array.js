aceb83675552081881b97d9c33a0c3d808dd8a9b
Hitesh Bhagwat <111442899+hiteshdevs@users.noreply.github.com>
2022-08-21 14:04:19 +0530

    Update Callback and arrow function introduction in array.js

--- Callback and arrow function introduction in array.js
+++ Callback and arrow function introduction in array.js
@@ -34,3 +34,27 @@ console.log(result);
 // 5
 var result = [2,4,6,8].every((e) => e %2 === 0);
 console.log(result);
+// ----------------------------------------------------------------------------
+
+function isEven(e){
+    return e % 2 === 0;
+}
+
+var rst = [2,4,6,8].every(isEven);
+console.log(rst);
+
+// callback
+
+var num = (element) =>{
+    return element % 2 === 0;
+}
+
+var rst = [2,4,6,8].every(num);
+console.log(rst);
+
+// arrow function callback
+
+var rlt = [2,4,61,8].every((ee) => {
+    return ee % 2 === 0;
+});
+console.log(rlt);
