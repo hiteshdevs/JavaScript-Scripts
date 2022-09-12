4dcdf0ca98c6cc2c9d23c13abac570e061662e7a
Hitesh Bhagwat <111442899+hiteshdevs@users.noreply.github.com>
2022-09-12 11:43:56 +0530

    Create age-checker.js

---
+++ age-checker.js
@@ -0,0 +1,12 @@
+console.log('----------------------------------------------------------');
+console.log('Age to have marriage');
+
+let name = prompt('pls enter name');
+let age = prompt('pls enter ur age');
+
+if (age >= 18) {
+   console.log("Ready for marriage");
+}
+else{
+  console.log("U r still kid get a candy for urself");
+}
